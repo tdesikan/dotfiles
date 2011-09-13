f2f1c30c747b98e633c1d92af61957930ddab914
Tarun Desikan <tarun@moovweb.com>
2011-09-12 18:01:48 -0700

    git clone not git checkout

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 if [ ! -d ${HOME}/.dotfiles ] ; then
   echo "NO DOTFILES - checking out for you"
   echo $HOME
-  cd ${HOME} && git checkout git@github.com:tdesikan/dotfiles.git .dotfiles
+  cd ${HOME} && git clone git@github.com:tdesikan/dotfiles.git .dotfiles
   cd ${HOME}/.dotfiles &&  git submodule update --init
 fi
 
